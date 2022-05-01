9d151cccbdb205db5d7d4bfed057fb69204ac495
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-05-01 17:09:17 -0300

    fix requirements/admin

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Produtos
+from .models import Produtos, Categoria
 
 @admin.register(Produtos)
 class ProdutosAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "size", "price")
     prepopulated_fields = {"slug": ("title",)}
+
+@admin.register(Categoria)
+class CategoriasAdmin(admin.ModelAdmin):
+    list_display = ("categoria",)
